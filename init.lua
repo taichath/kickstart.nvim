0680de48b10da92ceaa1964b8721141114f5605f
Taimoor Chatha <taichath@amazon.com>
2025-07-17 08:48:44 -0400

    add harpoon2

--- init.lua
+++ init.lua
@@ -939,6 +939,51 @@ require('lazy').setup({
       --  Check out: https://github.com/echasnovski/mini.nvim
     end,
   },
+  { -- Harpoon - quick file navigation
+    'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      local harpoon = require('harpoon')
+      harpoon:setup({})
+
+      -- basic telescope configuration
+      local conf = require("telescope.config").values
+      local function toggle_telescope(harpoon_files)
+          local file_paths = {}
+          for _, item in ipairs(harpoon_files.items) do
+              table.insert(file_paths, item.value)
+          end
+
+          require("telescope.pickers").new({}, {
+              prompt_title = "Harpoon",
+              finder = require("telescope.finders").new_table({
+                  results = file_paths,
+              }),
+              previewer = conf.file_previewer({}),
+              sorter = conf.generic_sorter({}),
+          }):find()
+      end
+
+      vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end,
+          { desc = "Open harpoon window" })
+      
+      -- Add file to harpoon
+      vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end, 
+          { desc = "Add file to harpoon" })
+      
+      -- Navigate to files in harpoon list
+      vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end, 
+          { desc = "Navigate to harpoon file 1" })
+      vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end, 
+          { desc = "Navigate to harpoon file 2" })
+      vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end, 
+          { desc = "Navigate to harpoon file 3" })
+      vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end, 
+          { desc = "Navigate to harpoon file 4" })
+    end,
+  },
+
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
